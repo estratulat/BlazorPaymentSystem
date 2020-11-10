9912e84e96f874c51a9a780f08f70c19da24366d
Eugeniu Stratulat <Eugeniu.Stratulat@endava.com>
2020-11-10 19:41:07 +0200

    implemented transfer

--- BlazorApp3/Server/Controllers/WalletController.cs
+++ BlazorApp3/Server/Controllers/WalletController.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using BlazorApp3.Server.Data;
 using BlazorApp3.Server.Models;
+using BlazorApp3.Shared;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Wallet = BlazorApp3.Server.Models.Wallet;
 
 namespace BlazorApp3.Server.Controllers
 {
@@ -31,6 +33,15 @@ namespace BlazorApp3.Server.Controllers
             var userId = userManager.GetUserId(User);
             var wallets = context.Users.Include(x => x.Wallets).FirstOrDefault(x => x.Id == userId).Wallets;
             return wallets;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public Wallet GetWallet(Guid id)
+        {
+            var userId = userManager.GetUserId(User);
+            var wallet = context.Users.Include(x => x.Wallets).FirstOrDefault(x => x.Id == userId).Wallets.FirstOrDefault(x => x.Id == id);
+            return wallet;
         }
 
         [HttpPost]
@@ -72,6 +83,45 @@ namespace BlazorApp3.Server.Controllers
 
             var wallet = context.Wallets.Find(id);
             context.Wallets.Remove(wallet);
+            context.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("transfer")]
+        public ActionResult MakeTransfer([FromBody] TransferDto data)
+        {
+            var userId = userManager.GetUserId(User);
+            var user = context.Users.Include(x => x.Wallets).FirstOrDefault(x => x.Id == userId);
+            if (!user.Wallets.Any(x => x.Id == Guid.Parse(data.SourceWalletId)))
+            {
+                return BadRequest();
+            }
+
+            var source = user.Wallets.FirstOrDefault(x => x.Id == Guid.Parse(data.SourceWalletId));
+
+            var destinantionUser = context.Users.Include(x => x.Wallets).FirstOrDefault(x => x.UserName == data.Username);
+
+            var destination = destinantionUser.Wallets.FirstOrDefault(x => x.Currency == data.Currency);
+
+            if(destination == null || source.Amount < data.Amount)
+            {
+                return BadRequest();
+            }
+
+            source.Amount -= data.Amount;
+            destination.Amount += data.Amount;
+
+            var transaction = new Transaction
+            {
+                Amount = data.Amount,
+                Date = DateTime.Now,
+                DestinationWalletId = destination.Id,
+                SourceWalletId = source.Id
+            };
+            context.Add(transaction);
+
             context.SaveChanges();
 
             return Ok();
